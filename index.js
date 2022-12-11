37a8f47f8d518b172d9c6d72178555878e8d0cc8
Looopees <lopess.andrade@gmail.com>
2022-12-11 09:36:34 +0000

    all brands call

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
 
+const BROWSER_HEADERS = {
+  headers: { "Accept-Encoding": "gzip,deflate,compress" },
+};
 
-const BROWSER_HEADERS = { 
-    headers: { "Accept-Encoding": "gzip,deflate,compress" } 
-}
+const BRANDS = [
+  "Apple",
+  "Samsung",
+  "Motorola",
+  "Xiaomi",
+  "Asus",
+  "Lenovo",
+  "Huawei",
+  "LG",
+];
 
 let phonesList = [];
 
-const getMotorola = async () => {
-    const BASE_URL = 'https://phonesdata.com/pt/smartphones/'
-    const pathMotorola = 'motorola/20'  // xx/ no final
-    for (let o=13;o <= 22;o++){
-    const url = BASE_URL + pathMotorola + o.toString() + '/'
-    let pageResquest = await axios.get(url, BROWSER_HEADERS);
-    const html = pageResquest.data
+const getData = async () => {
+  const BASE_URL = "https://phonesdata.com/pt/smartphones/";
+  for (let p = 0; p < BRANDS.length; p++) {
+    const pathBrand = BRANDS[p] + "/20"; // xx/ no final
+    for (let o = 12; o <= 22; o++) {
+      const url = BASE_URL + pathBrand + o.toString() + "/";
+      let pageResquest = await axios.get(url, BROWSER_HEADERS);
+      const html = pageResquest.data;
 
-    const $ = cheerio.load(html);
-    for (let i=0;i<=50;i++){
-        const selectorImg = `#main > div > div > div.col-md-9.col-sm-7 > div > div:nth-child(10) > div > div > a > img`
-        const selectorTitle = `#main > div > div > div.col-md-9.col-sm-7 > div > div:nth-child(${i}) > div > p > a`
-        if($(selectorTitle).text()){
-            const image = $(selectorImg).attr('data-ezsrc');
-            const title = $(selectorTitle).text();
-            phonesList.push( {brand: 'motorola', model: title, img: image})
+      const $ = cheerio.load(html);
+      for (let i = 0; i <= 50; i++) {
+        const selectorImg = `#main > div > div > div.col-md-9.col-sm-7 > div > div:nth-child(${i}) > div > div > a > img`;
+        const selectorTitle = `#main > div > div > div.col-md-9.col-sm-7 > div > div:nth-child(${i}) > div > p > a`;
+        if ($(selectorTitle).text()) {
+          const image = $(selectorImg).attr("data-ezsrc");
+          const title = $(selectorTitle).text();
+          phonesList.push({ brand: BRANDS[p], model: title, img: image });
         }
+      }
+      console.log("pagina " + o + " de " + BRANDS[p] + " concluida");
     }
-console.log('pagina'+o+'concluida')
-
-};
-fs.writeFile('phonesList.json', 
-                          JSON.stringify(phonesList, null, 4), 
-                          (err)=> console.log('File successfully written!'))
-//console.log(phonesList);
-phonesList = [];
-console.log(phonesList,'final')
+  }
+  fs.writeFile("phonesList.json", JSON.stringify(phonesList, null, 4), (err) =>
+    console.log("File successfully written!")
+  );
+  //console.log(phonesList);
+  phonesList = [];
+  console.log(phonesList, "final");
 };
 
-getMotorola();
+getData();
