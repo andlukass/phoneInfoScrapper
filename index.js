c34ab4b2720559d992b93671ab3d74deb3408200
andlukass <lopess.andrade@gmail.com>
2024-05-25 09:23:16 +0100

    refac script

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const path = require('path');
 const fs = require("fs");
+
+// const filePath = path.join(__dirname, './iphone.html');
 
 const BROWSER_HEADERS = {
   headers: { "Accept-Encoding": "gzip,deflate,compress" },
@@ -20,33 +23,34 @@ const BRANDS = [
 let phonesList = [];
 
 const getData = async () => {
-  const BASE_URL = "https://phonesdata.com/pt/smartphones/";
-  for (let p = 0; p < BRANDS.length; p++) {
-    const pathBrand = BRANDS[p] + "/20"; // xx/ no final
-    for (let o = 12; o <= 22; o++) {
-      const url = BASE_URL + pathBrand + o.toString() + "/";
+  // const html = fs.readFileSync(filePath, 'utf8');
+  const BASE_URL = "https://phonesdata.com/en/smartphones/";
+  for (const brand of BRANDS) {
+      const url = BASE_URL + brand;
       let pageResquest = await axios.get(url, BROWSER_HEADERS);
       const html = pageResquest.data;
 
       const $ = cheerio.load(html);
-      for (let i = 0; i <= 50; i++) {
-        const selectorImg = `#main > div > div > div.col-md-9.col-sm-7 > div > div:nth-child(${i}) > div > div > a > img`;
-        const selectorTitle = `#main > div > div > div.col-md-9.col-sm-7 > div > div:nth-child(${i}) > div > p > a`;
-        if ($(selectorTitle).text()) {
-          const image = $(selectorImg).attr("data-ezsrc");
-          const title = $(selectorTitle).text();
-          phonesList.push({ brand: BRANDS[p], model: title, img: image });
-        }
-      }
-      console.log("pagina " + o + " de " + BRANDS[p] + " concluida");
-    }
+      $('h3').each((index, element) => {
+        const idValue = parseInt($(element).attr('id'), 10);
+        if (idValue < 2014) return false;
+        $(element).closest('.col-md-12')
+        .nextAll()
+        .each((i, nextElement) => {
+          const nextDiv = $(nextElement);
+          if (!(nextDiv.hasClass('col-md-2') && nextDiv.hasClass('col-sm-3') && nextDiv.hasClass('col-xs-3'))) return false;
+            const model = nextDiv.find('span').text();
+            const imgSrc = nextDiv.find('img').attr('data-ezsrc');
+            console.log(imgSrc);
+            console.log(model);
+            phonesList.push({ model: model, img: imgSrc });
+        });
+      });
+      console.log(brand + " concluida");
   }
   fs.writeFile("phonesList.json", JSON.stringify(phonesList, null, 4), (err) =>
     console.log("File successfully written!")
   );
-  //console.log(phonesList);
-  phonesList = [];
-  console.log(phonesList, "final");
 };
 
 getData();
